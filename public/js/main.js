9d88d020d836a38b48c5ba8ed3e27be4ab03bce1
Tinfoilboy <tinfoilboy@gmail.com>
2014-07-26 21:53:32 -0400

    Made regeneration faster (hopefully) by getting a new hashtag from the API instead of the homepage, lessening the hashtag load times.

--- public/js/main.js
+++ public/js/main.js
@@ -17,25 +17,28 @@ $(function() {
 			$('title').text(hashtag + ' - Random Hashtag Generator');
 			$('.hashtag').text(hashtag);
 		}).always(function() {
+			console.log("this should be executed afterwards");
 			$('.social.buttons').find('#permalink').attr('data-clipboard-text', 'http://hashtag.tinfoilboy.com/' + window.location.hash);
 			$('.social.buttons').find('.twitter-share-button').attr('data-url', 'http://hashtag.tinfoilboy.com/' + window.location.hash);
 		});
 	}
 	$(document).on('click', '#try-again', function(event) {
 		event.preventDefault();
-		$('.social.buttons').slideUp('slow');
-		$.ajax({
-			url: document.URL,
-			type: 'GET',
-			dataType: 'html',
-		}).done(function(data) {
-			console.log($(data).filter('.vertical').find('.hashtag'));
-			$('title').text($(data).filter('.vertical').find('.hashtag').text() + ' - Random Hashtag Generator')
-			$('.hashtag').replaceWith($(data).filter('.vertical').find('.hashtag'));
-			$('#permalink').attr('data-clipboard-text', $(data).filter('.vertical').find('.social.buttons').find('#permalink').attr('data-clipboard-text'));
-			$('.hashtag').slideDown('slow');
-			$('.social.buttons').slideDown('slow');
-			twttr.widgets.load();
+		$('.hashtag').slideUp('slow');
+		$('.social.buttons').slideUp('slow', function() {
+			$.ajax({
+				url: (document.URL.indexOf('profanity=1') != -1) ? '/api/hashtag/generate/profane/' : '/api/hashtag/generate/',
+				type: 'GET',
+				dataType: 'json',
+			}).done(function(data) {
+				$('title').text(data.hashtag + ' - Random Hashtag Generator');
+				$('.hashtag').text(data.hashtag);
+				$('#permalink').attr('data-clipboard-text', data.permalink);
+			}).always(function() {
+				$('.hashtag').slideDown('slow');
+				$('.social.buttons').slideDown('slow');
+				twttr.widgets.load();
+			});
 		});
 	});
 	$('.hashtag').slideDown('slow');
